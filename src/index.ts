456df918af8627a1622b302f2d23cba077654c01
mxdvl <hi@mxdvl.com>
2024-03-01 10:04:03 +0000

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,21 @@
+import { parse, object, string, number } from 'valibot';
+
+const schema = object({
+  name: string(),
+  age: number(),
+});
+
+function component() {
+  const element = document.createElement('div');
+
+  const { name, age } = parse(schema, {
+    name: 'Valibot',
+    age: 1,
+  });
+
+  element.innerHTML = `Hi, I’m ${name} and ${age} years old!`;
+
+  return element;
+}
+
+document.body.appendChild(component());
